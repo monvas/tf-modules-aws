65b5fddcf69799151124cd7d3de9058f76e69624
monvas <monvas-3@student.ltu.se>
2023-11-21 15:44:45 +0100

    update main and outputs for ec2

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 module "s3_bucket" {
   source = "./s3/tf-s3-bucket"
   providers = {
@@ -15,3 +16,36 @@ module "s3_object_upload" {
     local_path = "./Test"
     object_files = ["cat4.jpg","dog4.jpg"]
 }
+*/
+
+/* 
+    Creates EC2 instance
+*/
+module "ec2_instance" {
+  source = "./ec2/tf-create-instance"
+  providers = {
+    aws = aws.en1
+  }
+  key_name = "ec2-keypair"
+  security_groups = ["sg-03a4f6e5db55bfe95"]
+}
+
+/* 
+    Updates previous EC2 instance to stopped state
+*/
+module "ec2_state" {
+    source = "./ec2/tf-instance-state"
+  providers = {
+    aws = aws.en1
+  }
+  instance_id = module.ec2_instance.instance_id[0]
+  state = "stopped"
+}
+
+# module "ec2_list" {
+#     source = "./ec2/tf-list-instances"
+#       providers = {
+#         aws = aws.en1
+#     }
+# }
+
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
-output "bucket_id" {
-  value = module.s3_bucket.bucket_id
+# output "bucket_id" {
+#   value = module.s3_bucket.bucket_id
+# }
+
+# output "ec2_ids" {
+#   value = module.ec2_list.ec2_id
+# }
+
+# output "ec2_region" {
+#   value = module.ec2_list.ec2_region
+# }
+
+# Displays instance state
+output "instance_state" {
+  value = module.ec2_instance.instance_state
 }
